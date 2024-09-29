c7eb9287585ac72e4c3733020f1b7c00f8ed7139
Marudau, Dzmitry <Dzmitry.Marudau@McKesson.com>
2024-09-30 01:02:37 +0300

    Add Spark dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,22 @@ repositories {
 }
 
 dependencies {
+    //Apache Spark uses Log4j for logging, while Spring Boot uses SLF4J. The presence of both
+    // log4j-slf4j2-impl and log4j-to-slf4j causes a conflict
+    // because they both try to bridge Log4j and SLF4J in different ways.
+    implementation("org.apache.spark:spark-core_2.13:3.5.3") {
+        exclude(group = "org.slf4j", module = "slf4j-log4j12")
+        exclude(group = "org.slf4j", module = "slf4j-api")
+    }
+    implementation("org.apache.spark:spark-sql_2.13:3.5.3") {
+        exclude(group = "org.slf4j", module = "slf4j-log4j12")
+        exclude(group = "org.slf4j", module = "slf4j-api")
+    }
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("ch.qos.logback:logback-classic:1.5.8")
+
     implementation("org.springframework.boot:spring-boot-starter")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
