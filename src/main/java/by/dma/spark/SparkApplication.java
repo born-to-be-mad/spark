d57d46441750ece6d95ec5841b7a43e34363c9a9
Marudau, Dzmitry <Dzmitry.Marudau@McKesson.com>
2024-09-30 01:00:53 +0300

    Simple SpringBoot console application

---
+++ src/main/java/by/dma/spark/SparkApplication.java
@@ -0,0 +1,25 @@
+package by.dma.spark;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class SparkApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        log.info("STARTING...");
+        SpringApplication.run(SparkApplication.class, args);
+        log.info("DONE");
+    }
+
+    @Override
+    public void run(String... args) {
+        log.info("Running ith arguments...");
+        for (int i = 0; i < args.length; ++i) {
+            log.info("args[{}]: {}", i, args[i]);
+        }
+    }
+}
